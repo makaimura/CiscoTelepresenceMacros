70ce669039009b5c8ce300fbd0a252da7f702bc1
Alice Makaimura <arumumu@live.jp>
2018-04-17 10:24:36 +0900

    Update twotapdial.js

--- Two-Tap Dial/twotapdial.js
+++ Two-Tap Dial/twotapdial.js
@@ -1,1 +1,16 @@
 const xapi = require('xapi');
+
+const speedList = (contact) => ({
+  "FavCall_1": "user1@brah.webex.com",
+  "FavCall_2": "user2@brah.webex.com",
+  "FavCall_3": "user3@brah.webex.com",
+  "FavCall_4": "user4@brah.webex.com",
+  "FavCall_5": "user5@brah.webex.com"
+})[contact];
+
+xapi.event.on('UserInterface Extensions Widget Action', (event) => {
+  if(event.Type == 'clicked'){
+    var contactNumber = speedList(event.WidgetId);
+    xapi.command('dial', {Number: contactNumber});
+  }
+});
